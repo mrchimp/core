9f31cd5d4fb6ed369a846f6958c9c374825fd178
Daniel Hewes <daniel@danimalweb.co.uk>
2011-11-04 12:59:51 +1100

    Email Const and missing ;

--- db_con/localhost-example.php
+++ db_con/localhost-example.php
@@ -1,6 +1,6 @@
 <?php
-define('DBUSER', 'XXX')
+define('DBUSER', 'XXX');
 define('DBPASSWORD', 'XXX');
 define('DSN', 'mysql:host=XXX;dbname=XXX');
 define('LOG_FILE', 'C:\logs\mylog.log');
-
+define('EMAIL', 'you@youremail.com');
